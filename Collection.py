38eba13ea4800de0ab12dd71c084928acdda98c5
ntheobald <76193349+ntheobald@users.noreply.github.com>
2021-09-12 13:59:38 -0700

    Update Collection.py
    MAKE SURE TO ADD TOKEN WHEN RUNNING

--- Collection.py
+++ Collection.py
@@ -1,1 +1,23 @@
-#bot token= ODg2NzA0MjMzODkyNzA4Mzg0.YT5dqw.-Gc6vlrozeNu2xcewDMQbpUPOG8
+import discord
+import os
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+    
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        if message.content == 'mom':
+            await message.channel.send('HAHAHAHA YOUR MOM HAS A MASSIVE WILLY')
+        if message.content =='ping':
+            await message.channel.send('PING PONG MOTHERFUCKER')
+
+client = MyClient()
+client.run('ADD TOKEN HERE!')
+
+
+
+
